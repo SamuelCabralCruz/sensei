276482e2c2f4c3d97997add206d79b57d0c7b613
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2020-03-30 20:42:05 -0400

    attempt to publish plugin

--- src/main/kotlin/ca/ulaval/glo/persistence/review/ReviewPersistence.kt
+++ src/main/kotlin/ca/ulaval/glo/persistence/review/ReviewPersistence.kt
@@ -3,10 +3,9 @@ package ca.ulaval.glo.persistence.review
 import ca.ulaval.glo.persistence.review.state.Review
 import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.ServiceManager
-import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 
-@State(name = "ReviewPersistence", storages = [Storage("review.xml")])
+@Storage("review.xml")
 class ReviewPersistence : PersistentStateComponent<Review> {
     private var stateValue = Review()
 
